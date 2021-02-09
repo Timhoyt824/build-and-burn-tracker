f35c548719a5bd6375d028d2befe0dbebb0451d7
Tim Hoyt <timhoyt824@gmail.com>
2021-02-09 11:11:33 -0800

    deploying

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log(`Successfully connected to database: ${databaseName}`))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => console.log(`Successfully connected to database: ${databaseName}`))
 
 
 app.use("/api", require("./routes/apiRoutes.js"));
